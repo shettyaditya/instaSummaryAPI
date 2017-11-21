a80b1c6efd9e6f19c52b08260f6986ef2357551f
Aditya Shetty <shettyaditya9879@gmail.com>
2017-11-21 15:29:10 +0530

    Few Changes

--- summarizer/views.py
+++ summarizer/views.py
@@ -30,6 +30,26 @@ class urlList(APIView):
 
     def post(self, request):
         posturl = request.data['url']  # url to be summarized
+        sites = ['youtube', 'angel', 'twitter', 'hackerearth', 'salesforce', 'linkedin', 'facebook', 'instagram',
+                 'stackoverflow', 'hackerrank', 'google']
+        for s in sites:
+            if posturl.__contains__(s):
+                siteerror = "This site cannot be summarized"
+                return Response(siteerror)
+
+        if posturl.endswith("pdf") or posturl.endswith("img") or posturl.endswith("jpeg") or posturl.endswith(
+                "jpg") or posturl.endswith("gif") or posturl.endswith("png") or posturl.endswith(
+                "ico") or posturl.endswith("svg") or posturl.endswith("xml") or posturl.endswith(
+                "mkv") or posturl.endswith("flv") or posturl.endswith("gif") or posturl.endswith(
+                "avi") or posturl.endswith("mp4") or posturl.endswith("mpv") or posturl.endswith(
+                "m4p") or posturl.endswith("svi") or posturl.endswith("dll") or posturl.endswith("exe"):
+            formaterror = "This is a file. If it is pdf,doc or a text file, please upload it in our site and get a summary"
+            return Response(formaterror)
+        s1 = 'chrome://newtab/'
+
+        if posturl.__contains__(s1):
+            emptytaberror = "This is an empty tab. Open a website to get a summary"
+            return Response(emptytaberror)
         parser = HtmlParser.from_url(posturl, Tokenizer(LANGUAGE))
         # or for plain text files
         # parser = Plaintext1Parser.from_file("document.txt", Tokenizer(LANGUAGE))
